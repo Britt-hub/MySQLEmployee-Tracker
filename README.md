e24966d6f92850019fee967d00c3768b55befb96
Brittany <britloy91@gmail.com>
2020-01-11 15:57:30 -0500

    Update JS

--- README.md
+++ README.md
@@ -12,6 +12,13 @@ This is an interface that makes it easy for business owner or employees to view 
 Inquirer Package Manager [pip] (https://www.npmjs.com/package/inquirer/v/0.2.3)
 
 
+HTTPS:
+$ git clone https://britt-hub.github.io/MySQLEmployee-Tracker/
+
+##Built With
+
+Node.js JavaScript runtime
+MySql - Data Base
 
 
 ## License
--- employeeTracker.sql
+++ employeeTracker.sql
@@ -1,14 +1,33 @@
-DROP DATABASE IF EXISTS ice_creamDB;
+DROP DATABASE IF EXISTS employdb;
 
-CREATE DATABASE ice_creamDB;
+CREATE DATABASE employdb;
 
-USE ice_creamDB;
-
-CREATE TABLE products (
+USE employdb;
+-- I TOOK OUT THE FOREIGN KEY
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  flavor VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NULL,
+  manger_id INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY(role_id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  
   PRIMARY KEY (id)
 );
 
@@ -21,6 +40,8 @@ VALUES ("chocolate", 3.10, 120);
 INSERT INTO products (flavor, price, quantity)
 VALUES ("strawberry", 3.25, 75);
 
+
+SELECT * FROM employee, role, department
 -- ### Alternative way to insert more than one row
 -- INSERT INTO products (flavor, price, quantity)
 -- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
